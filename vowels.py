5bd77ab3024fccb872585d92ca4b5ceaae4a9a77
amaanlakdawala <amaanlakdawala301@gmail.com>
2024-03-12 23:30:44 +0530

    just added

---
+++ vowels.py
@@ -0,0 +1,39 @@
+
+# name=input("Enter a String: ")
+# name=name.lower()
+# length=len(name)
+# for i in range(length):
+#     if name[i]=="a" or name[i]=="e" or name[i]=="i" or name[i]=="o" or name[i]=="u":
+#         name1=name.replace(name[i],"")
+# print("String after removing vowels: ",name1)  
+
+
+# list1=list(range(1,11))
+# even_list=[]
+# odd_list=[]
+# for i in list1:
+#     if i%2==0:
+#         even_list.append(i)
+#     else:
+#         odd_list.append(i)
+# print("Original List: ",list1)
+# print("Even List: ",even_list)
+# print("Odd List: ",odd_list)
+
+
+# dict1={"amaan":"9892662324","adnaan":"9769584262","anas":"98920112272"}
+# sorted_dict=dict(sorted(dict1.items()))
+# name=input("Enter your Name: ")
+# if name in sorted_dict.keys():
+#     print(" you are already present")
+# else:
+#     phone=input("enter phone number")
+#     sorted_dict[name]=phone
+# sorted_dict=dict(sorted(sorted_dict.items()))  
+# print("Final Sorted dictionary is: ",sorted_dict)
+
+
+
+
+
+    
